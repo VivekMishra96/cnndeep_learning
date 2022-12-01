f3e0ff6f36bc1eb401e8f00691139f9a791fd6c8
vivek mishra <starvivek1996@gmail.com>
2022-12-01 11:52:05 +0530

    data ingestion completed

--- src/deepClassifier/components/data_ingestion.py
+++ src/deepClassifier/components/data_ingestion.py
@@ -2,17 +2,27 @@ import os
 import urllib.request as request
 from zipfile import ZipFile
 from deepClassifier.entity import DataIngestionConfig
+from deepClassifier import logger
+from deepClassifier.utils import get_size
+from tqdm import tqdm
+from pathlib import Path
+
 
 class DataIngestion:
     def __init__(self, config: DataIngestionConfig):
         self.config = config
 
     def download_file(self):
+        logger.info("Trying to download file...")
         if not os.path.exists(self.config.local_data_file):
+            logger.info("Download started...")
             filename, headers = request.urlretrieve(
-                url = self.config.source_URL,
-                filename = self.config.local_data_file
+                url=self.config.source_URL,
+                filename=self.config.local_data_file
             )
+            logger.info(f"{filename} download! with following info: \n{headers}")
+        else:
+            logger.info(f"File already exists of size: {get_size(Path(self.config.local_data_file))}")        
 
     def _get_updated_list_of_files(self, list_of_files):
         return [f for f in list_of_files if f.endswith(".jpg") and ("Cat" in f or "Dog" in f)]
@@ -23,11 +33,16 @@ class DataIngestion:
             zf.extract(f, working_dir)
         
         if os.path.getsize(target_filepath) == 0:
+            logger.info(f"removing file:{target_filepath} of size: {get_size(Path(target_filepath))}")
             os.remove(target_filepath)
 
     def unzip_and_clean(self):
+        logger.info(f"unzipping file and removing unawanted files")
         with ZipFile(file=self.config.local_data_file, mode="r") as zf:
             list_of_files = zf.namelist()
             updated_list_of_files = self._get_updated_list_of_files(list_of_files)
-            for f in updated_list_of_files:
-                self._preprocess(zf, f, self.config.unzip_dir)        
+            for f in tqdm(updated_list_of_files):
+                self._preprocess(zf, f, self.config.unzip_dir)
+
+    
+    
