99916166668d6096ffc14f2fdb24882a81bc9919
vivek mishra <starvivek1996@gmail.com>
2022-12-01 10:44:00 +0530

    tested successfully

---
+++ src/deepClassifier/components/data_ingestion.py
@@ -0,0 +1,33 @@
+import os
+import urllib.request as request
+from zipfile import ZipFile
+from deepClassifier.entity import DataIngestionConfig
+
+class DataIngestion:
+    def __init__(self, config: DataIngestionConfig):
+        self.config = config
+
+    def download_file(self):
+        if not os.path.exists(self.config.local_data_file):
+            filename, headers = request.urlretrieve(
+                url = self.config.source_URL,
+                filename = self.config.local_data_file
+            )
+
+    def _get_updated_list_of_files(self, list_of_files):
+        return [f for f in list_of_files if f.endswith(".jpg") and ("Cat" in f or "Dog" in f)]
+
+    def _preprocess(self, zf: ZipFile, f: str, working_dir: str):
+        target_filepath = os.path.join(working_dir, f)
+        if not os.path.exists(target_filepath):
+            zf.extract(f, working_dir)
+        
+        if os.path.getsize(target_filepath) == 0:
+            os.remove(target_filepath)
+
+    def unzip_and_clean(self):
+        with ZipFile(file=self.config.local_data_file, mode="r") as zf:
+            list_of_files = zf.namelist()
+            updated_list_of_files = self._get_updated_list_of_files(list_of_files)
+            for f in updated_list_of_files:
+                self._preprocess(zf, f, self.config.unzip_dir)        
---
+++ src/deepClassifier/entity/__init__.py
@@ -0,0 +1,3 @@
+
+from deepClassifier.entity.config_entity import DataIngestionConfig
+   
