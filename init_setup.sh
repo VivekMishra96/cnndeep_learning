9636666d2667e27a81e12c653bb622e8c8acb44a
vivek mishra <starvivek1996@gmail.com>
2022-12-10 19:53:51 +0530

    commited

--- init_setup.sh
+++ init_setup.sh
@@ -1,6 +1,6 @@
 echo [$(date)]: "START" 
-echo [$(date)]: "creating env with python 3.8 version" 
-conda create --prefix ./env python=3.8 -y
+echo [$(date)]: "creating env with python 3.8.15 version" 
+conda create --prefix ./env python=3.8.15 -y
 echo [$(date)]: "activating the environment" 
 source activate ./env
 echo [$(date)]: "installing the dev requirements" 
